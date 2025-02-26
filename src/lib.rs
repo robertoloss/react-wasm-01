e898033254922f894becdb8ac0d484988412db9e
Roberto Loss <robertoloss@gmail.com>
2025-02-25 20:25:38 -0500

    dies if hits the tail

--- src/lib.rs
+++ src/lib.rs
@@ -619,6 +619,23 @@ pub fn render_game(ctx: &CanvasRenderingContext2d) {
                 tile.occupied = Occupied::Empty;
             }
         } 
+        let curr_tile_role = &tiles_map
+            .get(&player.curr_pos)
+            .expect("No tile found")
+            .occupied;
+        if *curr_tile_role == Occupied::Tail || *curr_tile_role == Occupied::Enemy {
+            player.curr_pos = player.init_pos.clone();
+            tiles_map
+                .iter_mut()
+                .for_each(|tile| {
+                    if tile.1.occupied == Occupied::Tail && tile.1.role != Role::Border {
+                        tile.1.role = Role::Board;
+                        tile.1.occupied = Occupied::Empty;
+                    }
+                });
+            player.tail = vec![];
+            player.movement = Move::Still;
+        }
         tiles_map
             .get_mut(&player.curr_pos)
             .expect("No tile found")
